510c8f3d3380c2de529fd0d4bc8a56cb3cbc2666
Alex Pitzel <pitzelalex@gmail.com>
2022-09-14 16:35:14 -0400

    Add challenge 2 answer

--- each.rb
+++ each.rb
@@ -8,10 +8,19 @@ weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
 
 weather_options.each do |weather_option|
   puts "Today the weather will be #{weather_option}"
-end 
+end
 
 # Challenge 2
 # Use the each method to print out the words that are exactly 3 characters in length
 words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
 
+words.each do |word|
+  if word.length == 3
+    puts word
+  elsif word.length != 3
+  end
+end
+
 # Record yourself live-coding and talking through either Challenge 1 or 2. The recording doesn't have to be the first time you are solving the problem. Use technical vocabulary and explain each part to demonstrate your understanding! Share the link to your video in your small group channel.
+
+# https://www.loom.com/share/8fcdcabd7932484c8b41dcb8c9002721
